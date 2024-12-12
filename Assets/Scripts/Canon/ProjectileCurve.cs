316fc1119957726984bcea338da171e5084d7728
GpasDeBlase <pierre.erme@gmail.com>
2024-12-12 11:42:50 +0100

    Commit jeudi matin
    Le canon : script quadraticCurve et celui qui fait que le projectile le suive

---
+++ Assets/Scripts/Canon/ProjectileCurve.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ProjectileCurve : MonoBehaviour
+{
+    public QuadraticCurve curve;
+    public float speed;
+
+    private float sampleTime;
+
+    void Start()
+    {
+        sampleTime = 0f;
+    }
+
+    void Update ()
+    {
+        sampleTime += Time.deltaTime * speed;
+        transform.position = curve.evaluate(sampleTime);
+        transform.forward = curve.evaluate(sampleTime + 0.001f) - transform.position;
+
+        if (sampleTime >= 1f)
+        {
+            Debug.Log("boom");
+            Destroy(gameObject);
+        }
+    }
+}
