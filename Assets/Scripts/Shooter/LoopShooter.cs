ab1ca7ebc32d026b07b0441bcb8424e7da60924a
Lyshaka <maxence.daboville@outlook.fr>
2024-12-17 14:59:49 +0100

    Added shoot offset on projectile shooter

--- Assets/Scripts/Shooter/LoopShooter.cs
+++ Assets/Scripts/Shooter/LoopShooter.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent (typeof(ProjectileShooter))]
 public class LoopShooter : MonoBehaviour
 {
 	public float shootFrequency = 5f;
+	public float offsetDelay = 0f;
 
 	protected float elapsedTime = 0f;
 	protected ProjectileShooter shooter;
+
+	protected bool started = false;
 
 	void Start()
 	{
@@ -15,11 +19,23 @@ public class LoopShooter : MonoBehaviour
 
 	protected void Update()
 	{
+		if (started)
+			PerformShoot();
+	}
+
+	protected void PerformShoot()
+	{
 		elapsedTime += Time.deltaTime;
 		if (elapsedTime >= 1f / shootFrequency)
 		{
 			shooter.ShootProjectile();
 			elapsedTime -= 1f / shootFrequency;
 		}
+	}
+
+	IEnumerator DelayStart()
+	{
+		yield return new WaitForSeconds(offsetDelay);
+		started = true;
 	}
 }
