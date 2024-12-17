1c53c626f2fbaeead54f80dd8740ca79c59368e3
Lyshaka <maxence.daboville@outlook.fr>
2024-12-17 14:35:52 +0100

    Added Tool scripts for turret movement

---
+++ Assets/Scripts/Shooter/Move.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Move : MonoBehaviour
+{
+	[SerializeField, Tooltip("Default to this transform if null")] protected Transform target;
+	[SerializeField, Tooltip("Delay before it starts its cycle")] float offsetDelay = 0f;
+
+	bool started = false;
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
+	{
+		Setup();
+	}
+
+	void Update()
+	{
+		if (started)
+			PerformMove();
+	}
+
+	protected virtual void Setup()
+	{
+		StartCoroutine(DelayStart());
+		if (target == null)
+			target = transform;
+	}
+
+	protected virtual void PerformMove()
+	{
+
+	}
+
+	IEnumerator DelayStart()
+	{
+		yield return new WaitForSeconds(offsetDelay);
+		started = true;
+	}
+}
---
+++ Assets/Scripts/Shooter/Translate.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Translate : Move
+{
+	[SerializeField] AnimationCurve movementCurve;
+	[SerializeField] float movementSpeed = 5f;
+	[SerializeField] Transform start;
+	[SerializeField] Transform end;
+
+	bool direction = true;
+	float distance;
+	float time;
+	float t;
+
+	protected override void Setup()
+	{
+		base.Setup();
+		distance = Vector3.Distance(start.position, end.position);
+		time = distance / movementSpeed;
+	}
+
+	protected override void PerformMove()
+	{
+		//Debug.Log("test");
+		if (direction)
+		{
+			t += Time.deltaTime;
+			if (t > time)
+				direction = false;
+		}
+		else
+		{
+			t -= Time.deltaTime;
+			if (t < 0f)
+				direction = true;
+		}
+		
+		target.position = Vector3.Lerp(start.position, end.position, movementCurve.Evaluate(t / time));
+	}
+}
