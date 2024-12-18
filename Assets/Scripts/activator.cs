72d6c4b40fd6f8f0d18d02362459c5f0e8edd9b4
GpasDeBlase <pierre.erme@gmail.com>
2024-12-18 12:02:25 +0100

    Commit mercredi matin
    fin du niveau blockout
    faut ajouter les ennemis et le coffre a la fin

--- Assets/Scripts/activator.cs
+++ Assets/Scripts/activator.cs
@@ -1,28 +1,43 @@
 using TMPro;
 using UnityEngine;
 
-public class activator : MonoBehaviour
+public class Activator : MonoBehaviour
 {
     public GameObject activable;
     public TextMeshPro tmp;
+    private bool inBox = false;
 
     void Start()
     {
+        tmp.enabled = false;
         
     }
 
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            activable.SetActive(false);
+        }
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 3 )
         {
-            tmp
-            
+            inBox = true;
+            tmp.enabled = true;          
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 3)
+        {
+            tmp.enabled = false;
+            inBox = false;
         }
     }
 }
