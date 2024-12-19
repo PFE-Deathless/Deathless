6c9df92c1a4e8fda3b42080d7b53894eb8d5d8ea
GpasDeBlase <pierre.erme@gmail.com>
2024-12-19 15:26:41 +0100

    commit pour etre sur pour que max merge

--- Assets/Scripts/CheatTP.cs
+++ Assets/Scripts/CheatTP.cs
@@ -1,16 +1,31 @@
+using NUnit.Framework;
 using UnityEngine;
 
 public class CheatTP : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject cam;
+    public Transform player;
+    public Transform[] listCheckpoint = new Transform[9];
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Keypad0)) player.position = listCheckpoint[0].position;
+        if (Input.GetKeyDown(KeyCode.Keypad1)) player.position = listCheckpoint[1].position;
+        if (Input.GetKeyDown(KeyCode.Keypad2)) player.position = listCheckpoint[2].position;
+        if (Input.GetKeyDown(KeyCode.Keypad3)) player.position = listCheckpoint[3].position;
+        if (Input.GetKeyDown(KeyCode.Keypad4)) player.position = listCheckpoint[4].position;
+        if (Input.GetKeyDown(KeyCode.Keypad5)) player.position = listCheckpoint[5].position;
+        if (Input.GetKeyDown(KeyCode.Keypad6)) player.position = listCheckpoint[6].position;
+        if (Input.GetKeyDown(KeyCode.Keypad7)) player.position = listCheckpoint[7].position;
+        if (Input.GetKeyDown(KeyCode.Keypad8)) player.position = listCheckpoint[8].position;
+
+
     }
 }
