1f9c352bc1a5dfbb5c8a0306ed57664f7d85c064
GpasDeBlase <pierre.erme@gmail.com>
2024-12-19 11:42:31 +0100

    commit avant merge

--- Assets/Scripts/activator.cs
+++ Assets/Scripts/activator.cs
@@ -1,31 +1,46 @@
 using TMPro;
 using UnityEngine;
 
+
 public class Activator : MonoBehaviour
 {
     public GameObject activable;
     public TextMeshPro tmp;
     private bool inBox = false;
+    private bool canInteract = true;
+    public GameObject levier;
+    public GameObject _feedback;
 
     void Start()
     {
         tmp.enabled = false;
         
+
+
     }
 
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && inBox)
         {
             activable.SetActive(false);
+
+            // feedback
+            levier.transform.localRotation = Quaternion.Euler(0f, 0f, 30f);
+            ParticleSystem ps = _feedback.GetComponent<ParticleSystem>();
+            ps.Play();
+
+            // l'objet n'est plus utilisable
+            canInteract = false;
+
         }
 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == 3 )
+        if (other.gameObject.layer == 3 && canInteract)
         {
             inBox = true;
             tmp.enabled = true;          
