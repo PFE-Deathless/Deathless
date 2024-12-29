fc602a4a38c3eab98b6e5b6591960ab0d18d00d1
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2024-12-29 16:00:35 +0100

    Updated Health display and Game manager for the UI to be updated when player takes damage

--- Assets/Scripts/UI/HealthDisplay.cs
+++ Assets/Scripts/UI/HealthDisplay.cs
@@ -5,8 +5,8 @@ using UnityEngine.UI;
 public class HealthDisplay : MonoBehaviour
 {
 	// ######## STATISTICS ########
-	public int health;
-	public int maxHealth;
+	//public int health;
+	//public int maxHealth;
 
 	[Header("Appearance")]
 
@@ -14,25 +14,23 @@ public class HealthDisplay : MonoBehaviour
 	public Sprite fullHeart;
 	public Image[] hearts;
 
-	[Header("References")]
-	public PlayerHealth playerHealth;
+	//[Header("References")]
+	//public PlayerHealth playerHealth;
 
 
-	void Start()
+	//void Start()
+	//{
+
+	//}
+
+	public void UpdateHealth(int value) // La valeur passée en paramètre est la quantité de HP actuel du joueur
 	{
-		playerHealth.health = health;
-		playerHealth.healthMax = maxHealth;
-	}
-
-	void Update()
-	{
-		// pour debug :emojinerd:
-		//playerHealth.health = health;
-		//playerHealth.healthMax = maxHealth;
-
 		for (int i = 0; i < hearts.Length; i++)
 		{
-			if (i < playerHealth.health) // si il a de la vie les coeur sont pleins, sinon ils sont vides
+			hearts[i].sprite = i < value ? fullHeart : emptyHeart; // si il a de la vie les coeur sont pleins, sinon ils sont vides
+			/* équivalent à faire 
+			
+			if (i < value)
 			{
 				hearts[i].sprite = fullHeart;
 			}
@@ -41,7 +39,19 @@ public class HealthDisplay : MonoBehaviour
 				hearts[i].sprite = emptyHeart;
 			}
 
-			if ( i < playerHealth.healthMax) 
+			*/
+		}
+	}
+
+	public void UpdateHealthMax(int value) // La valeur passée en paramètre est la quantité de HP max du joueur
+	{
+		for (int i = 0; i < hearts.Length; i++)
+		{
+			hearts[i].enabled = i < value;
+
+			/* équivalent à faire 
+			
+			if (i < value)
 			{
 				hearts[i].enabled = true;
 			}
@@ -49,7 +59,37 @@ public class HealthDisplay : MonoBehaviour
 			{
 				hearts[i].enabled = false;
 			}
+
+			*/
 		}
 	}
+
+	//void Update()
+	//{
+	//	// pour debug :emojinerd:
+	//	//playerHealth.health = health;
+	//	//playerHealth.healthMax = maxHealth;
+
+	//	for (int i = 0; i < hearts.Length; i++)
+	//	{
+	//		if (i < playerHealth.health) // si il a de la vie les coeur sont pleins, sinon ils sont vides
+	//		{
+	//			hearts[i].sprite = fullHeart;
+	//		}
+	//		else
+	//		{
+	//			hearts[i].sprite = emptyHeart;
+	//		}
+
+	//		if ( i < playerHealth.healthMax) 
+	//		{
+	//			hearts[i].enabled = true;
+	//		}
+	//		else
+	//		{
+	//			hearts[i].enabled = false;
+	//		}
+	//	}
+	//}
 
 }
