4bf28572405bb220d78fc3a919f3fd8ccb2b7500
Lyshaka <maxence.daboville@outlook.fr>
2025-02-17 10:25:13 +0100

    Changed cheatTP, to work with the new teleport method

--- Assets/Scripts/Debug/CheatTP.cs
+++ Assets/Scripts/Debug/CheatTP.cs
@@ -1,68 +1,28 @@
-using NUnit.Framework;
 using UnityEngine;
 
 public class CheatTP : MonoBehaviour
 {
-    //public GameObject cam;
-    public Transform player;
-    public Transform[] listCheckpoint = new Transform[9];
-    private Vector3 offset;
+	public Transform[] listCheckpoint = new Transform[9];
 
-
-    void Start()
-    {
-        //offset = cam.GetComponent<CameraBehavior>().offset;
-    }
-
- 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            player.position = listCheckpoint[0].position;
-            //cam.transform.position = listCheckpoint[0].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad1))
-        {
-            player.position = listCheckpoint[1].position;
-            //cam.transform.position = listCheckpoint[1].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad2))
-        {
-            player.position = listCheckpoint[2].position;
-            //cam.transform.position = listCheckpoint[2].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad3))
-        {
-            player.position = listCheckpoint[3].position;
-            //cam.transform.position = listCheckpoint[3].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad4))
-        {
-            player.position = listCheckpoint[4].position;
-            //cam.transform.position = listCheckpoint[4].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad5))
-        {
-            player.position = listCheckpoint[5].position;
-            //cam.transform.position = listCheckpoint[5].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad6))
-        {
-            player.position = listCheckpoint[6].position;
-            //cam.transform.position = listCheckpoint[6].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad7))
-        {
-            player.position = listCheckpoint[7].position;
-            //cam.transform.position = listCheckpoint[7].position + offset;
-        }
-        if (Input.GetKeyDown(KeyCode.Keypad8))
-        {
-            player.position = listCheckpoint[8].position;
-            //cam.transform.position = listCheckpoint[8].position + offset;
-        }
-
-
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Keypad0))
+			PlayerController.Instance.Teleport(listCheckpoint[0].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad1))
+			PlayerController.Instance.Teleport(listCheckpoint[1].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad2))
+			PlayerController.Instance.Teleport(listCheckpoint[2].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad3))
+			PlayerController.Instance.Teleport(listCheckpoint[3].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad4))
+			PlayerController.Instance.Teleport(listCheckpoint[4].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad5))
+			PlayerController.Instance.Teleport(listCheckpoint[5].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad6))
+			PlayerController.Instance.Teleport(listCheckpoint[6].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad7))
+			PlayerController.Instance.Teleport(listCheckpoint[7].position, Vector3.zero);
+		if (Input.GetKeyDown(KeyCode.Keypad8))
+			PlayerController.Instance.Teleport(listCheckpoint[8].position, Vector3.zero);
+	}
 }
