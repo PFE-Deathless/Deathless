9e35ab253e2d8099ef7e3fca7660ae8ed8abbd9b
Lyshaka <maxence.daboville@outlook.fr>
2025-02-18 16:53:54 +0100

    Added main menu fade

--- Assets/Scripts/UserInterface/TItle/StartScript.cs
+++ Assets/Scripts/UserInterface/TItle/StartScript.cs
@@ -1,14 +1,33 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
-    public string SceneName;
+	[SerializeField] string levelPath;
+	[SerializeField] Image fadeImage;
+	[SerializeField] float fadeDuration = 0.2f;
 
-    public void LoadScene()
-    {
-        SceneManager.LoadScene(SceneName);
-    }
+	public void LoadScene()
+	{
+		StartCoroutine(LoadFirstScene());
+	}
 
+	IEnumerator LoadFirstScene()
+	{
+		float elapsedTime = 0f;
 
+		while (elapsedTime < fadeDuration)
+		{
+			fadeImage.color = new(0f, 0f, 0f, elapsedTime / fadeDuration);
+
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+
+		fadeImage.color = new(0f, 0f, 0f, 1f);
+
+		SceneManager.LoadSceneAsync(levelPath);
+	}
 }
