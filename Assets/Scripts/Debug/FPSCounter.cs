e366025012c1e37bf9953dde4bd37b6384906240
Lyshaka <maxence.daboville@outlook.fr>
2025-02-18 15:48:46 +0100

    Changed dash, Added fps counter

---
+++ Assets/Scripts/Debug/FPSCounter.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+	[SerializeField, Tooltip("Update per second")] float updateFrequency = 10f;
+
+	TextMeshProUGUI _fpsText;
+	float _elapsedTime;
+	float _updateDuration;
+
+	private void Start()
+	{
+		_fpsText = GetComponentInChildren<TextMeshProUGUI>();
+		_updateDuration = 1f / updateFrequency;
+	}
+
+	private void Update()
+	{
+		if (_elapsedTime < _updateDuration)
+		{
+			_elapsedTime += Time.deltaTime;
+		}
+		else
+		{
+			_elapsedTime = 0f;
+			_fpsText.text = (1f / Time.deltaTime).ToString("#.0");
+		}
+
+
+	}
+}
