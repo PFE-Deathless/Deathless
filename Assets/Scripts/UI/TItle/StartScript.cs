9520ccfba08e15c29600ebe479806850fe1077fd
poleukiru <soulanny.phiphak@gmail.com>
2025-02-20 17:27:06 +0100

    Title screen final final
    tout marche comme il faut avec le fade et tout

--- Assets/Scripts/UI/TItle/StartScript.cs
+++ Assets/Scripts/UI/TItle/StartScript.cs
@@ -1,16 +1,16 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartScript : MonoBehaviour
 {
-    public string SceneName;
+    [SerializeField] string levelPath;
+    [SerializeField] Image fadeImage;
+    [SerializeField] float fadeDuration = 0.2f;
+
     public GameObject canvaActive;
     public GameObject canvaDeactivate;
-
-    public void LoadScene()
-    {
-        SceneManager.LoadScene(SceneName);
-    }
 
     public void LoadCanva()
     {
@@ -21,6 +21,28 @@ public class StartScript : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void LoadScene()
+    {
+        StartCoroutine(LoadFirstScene());
+    }
+
+    IEnumerator LoadFirstScene()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            fadeImage.color = new(0f, 0f, 0f, elapsedTime / fadeDuration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = new(0f, 0f, 0f, 1f);
+
+        SceneManager.LoadSceneAsync(levelPath);
     }
 
 }
