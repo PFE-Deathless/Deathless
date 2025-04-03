647c9c146331a0f42eff051f68fa3eb574da6e54
Lyshaka <maxence.daboville@outlook.fr>
2025-04-03 19:24:27 +0200

    Added fading door

--- Assets/Scripts/Gameplay/TutoManager.cs
+++ Assets/Scripts/Gameplay/TutoManager.cs
@@ -1,75 +1,87 @@
 using UnityEngine;
 using System.Collections;
-using System;
 
 public class TutoManager : MonoBehaviour
 {
-    public static TutoManager Instance { get; private set; }
+	public static TutoManager Instance { get; private set; }
 
-    // Références
-    [Header("UIs")]
-    [SerializeField] GameObject[] infoUI;
+	// Références
+	[Header("UIs")]
+	[SerializeField] GameObject[] infoUI;
 
-    [Header("Autres objets a désactiver")]
-    [SerializeField] GameObject dummy;
-    [SerializeField] GameObject porteAOuvrir;
-    [SerializeField] GameObject[] ennemisFin;
-    [SerializeField] GameObject porteDeFin;
-
-
+	[Header("Autres objets a désactiver")]
+	[SerializeField] GameObject dummy;
+	[SerializeField] GameObject porteAOuvrir;
+	[SerializeField] GameObject[] ennemisFin;
+	[SerializeField] GameObject porteDeFin;
 
 
 
-    private void Awake()
-    {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
-    }
 
-    void Start()
-    {
-        // désactive toute les UI au lancement de la scène
-        for (int i = 0; i < infoUI.Length; i++)
-        {
-            infoUI[i].gameObject.SetActive(false);
-        }
-        // Active la première UI
-        StartCoroutine(Affichage());
-    }
 
-    void Update()
-    {
-        // Quand le dummy il meurt la première fois ça ouvre la suite
-        if (dummy.GetComponent<Dummy>().Died == true) DummyDies();
+	private void Awake()
+	{
+		if (Instance == null) Instance = this;
+		else Destroy(gameObject);
+	}
 
-        // Quand le joueur a tué tout les ennemis pour dévérouiller le porte finale
-        if (ennemisFin[0] == null && ennemisFin[1] == null && ennemisFin[2] == null && ennemisFin[3] == null) EndTuto();
+	void Start()
+	{
+		// désactive toute les UI au lancement de la scène
+		for (int i = 0; i < infoUI.Length; i++)
+		{
+			infoUI[i].gameObject.SetActive(false);
+		}
+		// Active la première UI
+		StartCoroutine(Affichage());
+	}
 
-    }
+	void Update()
+	{
+		// Quand le dummy il meurt la première fois ça ouvre la suite
+		if (dummy.GetComponent<Dummy>().Died == true) DummyDies();
 
-    public void Activate(int _id, bool state)
-    {
-            infoUI[_id].SetActive(state);
-    }
+		// Quand le joueur a tué tout les ennemis pour dévérouiller le porte finale
+		if (ennemisFin[0] == null && ennemisFin[1] == null && ennemisFin[2] == null && ennemisFin[3] == null) EndTuto();
 
-    IEnumerator Affichage()
-    {
-        yield return new WaitForSeconds(0.75f);
-        infoUI[0].SetActive(true);
-    }
+	}
 
-    void DummyDies()
-    {
-        porteAOuvrir.SetActive(false);
-        infoUI[1].SetActive(false);
+	public void Activate(int _id, bool state)
+	{
+		infoUI[_id].SetActive(state);
+		if (infoUI[_id].TryGetComponent(out IActivable activable))
+			activable.Activate();
+	}
 
-    }
+	IEnumerator Affichage()
+	{
+		yield return new WaitForSeconds(0.75f);
+		infoUI[0].SetActive(true);
+	}
 
-    void EndTuto()
-    {
-        porteDeFin.SetActive(false);
-        infoUI[5].SetActive(false);
-        infoUI[6].SetActive(true);
-    }
+	void DummyDies()
+	{
+		if (porteAOuvrir == null)
+			return;
+
+		//porteAOuvrir.SetActive(false);
+		infoUI[1].SetActive(false);
+		if (porteAOuvrir.TryGetComponent(out IActivable activable))
+			activable.Activate();
+
+	}
+
+	void EndTuto()
+	{
+		if (porteDeFin == null)
+			return;
+
+		infoUI[5].SetActive(false);
+		infoUI[6].SetActive(true);
+
+		if (porteDeFin.TryGetComponent(out IActivable activable))
+			activable.Activate();
+
+	}
 
 }
