b09b0b7cfb0ee0c78d0ce9e952e287ddeaf14329
Lyshaka <maxence.daboville@outlook.fr>
2025-05-06 11:35:28 +0200

    Updated door to be activated by unlocked dungeon

--- Assets/Scripts/Gameplay/Door.cs
+++ Assets/Scripts/Gameplay/Door.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IActivable
 {
+	[Header("Parameters")]
 	[SerializeField] float fadeDuration = 1f;
+	[SerializeField, Tooltip("Type of door (Lever : opened by a lever, Progression : Opened by a finished dungeon, Key : Opened by a key)")] Type doorType = Type.Lever;
+	[SerializeField, Tooltip("Dungeon that opens the door if finished (If Progression is selected in Door Type)")] Dungeon dungeonValidation;
 
 	Material _transparentMaterial;
 	Color _baseColor;
@@ -11,6 +14,9 @@ public class Door : MonoBehaviour, IActivable
 
 	private void Start()
 	{
+		if (doorType == Type.Progression && GameManager.Instance.IsUnlocked(dungeonValidation))
+			Destroy(gameObject);
+
 		_transparentMaterial = GetComponentInChildren<MeshRenderer>().material;
 		_baseColor = _transparentMaterial.GetColor("_Base_Color");
 	}
@@ -37,5 +43,12 @@ public class Door : MonoBehaviour, IActivable
 	public void Activate()
 	{
 		_activated = true;
+	}
+
+	public enum Type
+	{
+		Lever,
+		Progression,
+		Key,
 	}
 }
